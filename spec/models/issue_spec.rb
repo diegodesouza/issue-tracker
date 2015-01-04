c8487dfd458987b222f23b507516605f939b2a03
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-03 23:12:25 -0500

    modified tests all tests passing

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe Issue, :type => :model do
       expect(Issue.severities).to include('High')
     end
 
-    # it 'does not have valid severity when nil' do
-    #   issue = Issue.create(title: "Adam's Beard", description: "What a glorious beard!", category: 'Bug', severity: nil)
-    #   issue.valid?
-    #   expect(Issue.all.count).to eql(0)
-    # end
+    it 'does not have valid severity when nil' do
+      issue = Issue.create(title: "Adam's Beard", description: "What a glorious beard!", category_id: 0, severity: nil)
+      issue.valid?
+      expect(Issue.all.count).to eql(0)
+    end
 
     it 'belongs to category' do
       category = Category.create(name: "The Bearded One")
