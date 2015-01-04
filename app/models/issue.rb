ca6356856df48c2d00f51add2236e79def137263
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-03 21:54:43 -0500

    set up association add gem shoulda-matchers to gemfile g migrations for category

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,16 +3,11 @@ class Issue < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :category
 
-  def self.categories
-    [
-      'Bug',
-      'Feature Request',
-      'Customer Service'
-    ]
-  end
+  belongs_to :category,
+    inverse_of: :issues
 
   validates_presence_of :severity
-  
+
   def self.severities
     [
       'Low',
